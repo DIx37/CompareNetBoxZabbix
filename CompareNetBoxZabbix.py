969e86b141184185a799afa5142fd8d1f174aa9e
DIx37 <dkarnov@gmail.com>
2022-11-16 18:15:26 +0300

    Fix log and add var

--- CompareNetBoxZabbix.py
+++ CompareNetBoxZabbix.py
@@ -2,14 +2,14 @@
 # Dmitrii Karnov <dkarnov@gmail.com>
 
 
-from dcim.models import Device
+from dcim.models import Device, DeviceType, Region, Site, Location
 from extras.scripts import *
 from pyzabbix import ZabbixAPI
 
 
-Z_URL = "http://"
-Z_LOGIN = ""
-Z_PASS = ""
+Z_URL = "http://172.16.2.107"
+Z_LOGIN = "admin"
+Z_PASS = "Gjyzirb37"
 
 
 class GetProviderInfo(Script):
@@ -18,6 +18,34 @@ class GetProviderInfo(Script):
 		description = "Сравнение NetBox и Zabbix"
 		commit_default = False
 		job_timeout = 120
+
+
+	region = ObjectVar(
+		model = Region,
+		description = "Выберите Регион",
+		required = False
+	)
+
+
+	site = ObjectVar(
+		model = Site,
+		description = "Выберите Объект",
+		required = False
+	)
+
+
+	location = ObjectVar(
+		model = Location,
+		description = "Выберите Локацию",
+		required = False
+	)
+
+
+	device_type = ObjectVar(
+		model = DeviceType,
+		description = "Выберите Тип Устройства",
+		required = False
+	)
 
 
 	def run(self, data, commit):
@@ -29,19 +57,17 @@ class GetProviderInfo(Script):
 			z_devices = z.host.get(output=['name'])
 
 
+			log_info = "<b>Нет в Zabbix</b><p>"
 			for n_device in n_devices:
 				compare = False
 				for z_device in z_devices:
 					if n_device.name == z_device['name']:
 						compare = True
 				if compare == False:
-					self.log_info(f"{n_device.name} - Нет в Zabbix")
-				else:
-					self.log_info(f"{n_device.name} - Есть в Zabbix")
-
+					log_info += f"&nbsp;&nbsp;&nbsp;- {n_device.name}<p>"
+			self.log_info(log_info)
 
 
 			self.log_success("Скрипт успешно завершён")
-			return n_devices, z_devices
 		except Exception as err:
 			self.log_failure(f"Произошла ошибка:\n{err}")
